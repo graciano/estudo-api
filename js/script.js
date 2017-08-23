5617e2b4fbcee8c752be190512945795a244df61
Matheus Graciano Andrade <matheus.andrade@webedia-group.com>
2017-08-23 14:00:01 -0300

    getting generation with list of pokemon

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-  let coordinates;
   const API_URL = 'https://pokeapi.co/api/v2';
-  fetch(API_URL + '/pokemon/1', {
+  fetch(API_URL + '/generation/1', {
     mode: 'cors',
     redirect: 'follow',
   }).then( response => response.json() )
-    .then( pokemon => {
-      console.log(pokemon);
+    .then( generation => {
+      console.log(generation);
     });
 });
   
