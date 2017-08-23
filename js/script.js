2d6efc662f370c42a62f14e507271025dd447944
Matheus Graciano Andrade <matheus.andrade@webedia-group.com>
2017-08-23 14:14:49 -0300

    searching trough pokemon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 document.addEventListener("DOMContentLoaded", function(event) { 
+  let pokemons = [];
   const API_URL = 'https://pokeapi.co/api/v2';
   fetch(API_URL + '/generation/1', {
     mode: 'cors',
@@ -6,6 +22,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }).then( response => response.json() )
     .then( generation => {
       console.log(generation);
+      pokemons = generation['pokemon_species'];
     });
+
+  console.log(document.querySelector('#global-search'));
+
+  document.querySelector('#global-search').addEventListener('keyup', function(ev) {
+//    debounce( () => {
+      let results = [];
+      console.log(this);
+      let query = this.value;
+      for (let i=0; i<pokemons.length; i++) {
+        let p = pokemons[i];
+        if (p.name.search(query) !== -1) results.push(p);
+      }
+      console.log(results);
+//    }, 500);
+  });
 });
   
